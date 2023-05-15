e097544d69e129586af4855ae0a05377ffc4b8b7
Ubuntu <ubuntu@ip-172-31-87-201.ec2.internal>
2023-05-15 09:34:34 +0000

    changes are made

--- app.py
+++ app.py
@@ -2,31 +2,33 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = "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"
+openai.api_key = "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"
 
 def training_data(data):
-    return """please predict text as positive or negative.
-    text: you are a bad man
-    sentiment: negative
-    text: you are good man
-    sentiment: positive
-    text: {}
-    sentiment: """.format(data.capitalize())
+    return f"question: {data}\n" \
+           f"answer: "
+
+def generate_answer(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.1,
+        max_tokens=50
+    )
+    return response.choices[0].text.strip()
 
 @app.route('/', methods=("GET", "POST"))
 def index():
     if request.method == "POST":
         data = request.form["data"]
-        res = openai.Completion.create(ssh -i "authentication.pem" ubuntu@ec2-44-204-174-177.compute-1.amazonaws.com
-            model="text-davinci-003",
-            prompt=training_data(data),
-            temperature=0.1
-        )
-        return redirect(url_for("index", result=res.choices[0].text))
-    
+        prompt = training_data(data)
+        result = generate_answer(prompt)
+        return redirect(url_for("index", result=result))
+
     result = request.args.get("result")
     return render_template('index.html', result=result)
 
+
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=9090, debug=True)
+    app.run(host='0.0.0.0', port=9090, debug=True)
 
